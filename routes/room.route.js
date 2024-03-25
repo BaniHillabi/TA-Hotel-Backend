78674a51328a5e8d0fd369cea8b66d0e9f20a49b
BaniHillabi <ibanihillabi2007@gmail.com>
2024-03-25 20:59:26 +0700

    add room route

--- routes/room.route.js
+++ routes/room.route.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-const roomController = require("../controllers/customer.controller");
-app.get("/", roomController.getAllCustomer);
-app.post("/", roomController.addCust);
-app.post("/find", roomController.findCust);
-app.post("/find/:id", roomController.getCustById);
-app.put("/:id", roomController.updateCust);
-app.delete("/:id", roomController.deleteCust);
+const roomController = require("../controllers/kamar.controller");
+app.get("/", roomController.getAllRoom);
+app.post("/", roomController.addRoom);
+app.post("/find", roomController.findRoom);
+app.get("/find/:id", roomController.getRoomById);
+app.put("/:id", roomController.updateRoom);
+app.delete("/:id", roomController.deleteRoom);
 
 module.exports = app;
